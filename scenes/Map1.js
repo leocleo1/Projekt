538990cd895880bb5e94e0615622d64abf0c5aeb
leonischwanewede <Leoni090404@gmail.com>
2025-07-10 10:07:37 +0200

    wüsten block

--- scenes/Map1.js
+++ scenes/Map1.js
@@ -1,27 +1,30 @@
 export class Map1 extends Phaser.Scene {
-    constructor() {
-      super('Map1');
-    }
-
-    preload() {
-        this.load.image('Startbg', 'assets/Startbg.png');
-        this.load.image('Weiter', 'assets/Weiter.png');
-        this.load.image('E1', 'assets/E1.png');
-    }
-  
-
-    create() {
-      const startBackground = this.add.image(0, 0, 'Startbg').setOrigin(0, 0).setScale(0.9);
-      const E1 = this.add.image(window.innerWidth /2 , 420, 'E1').setScale(0.5);
-      const weiter = this.add.image(1300, 750, 'Weiter').setScale(0.25).setInteractive();
-
-      const gameWidth = window.innerWidth
-      const gameHeight = window.innerHeight;
-      startBackground.setDisplaySize(gameWidth, gameHeight);
-  
-      
-      weiter.on('pointerdown', () => {
-        this.scene.start('DesertLevel'); // Start the JungleLevel scene
-      });
-    }
+  constructor() {
+    super('Map1');
   }
+
+  preload() {
+      this.load.image('Startbg', 'assets/Startbg.png');
+      this.load.image('Weiter', 'assets/Weiter.png');
+      this.load.image('E1', 'assets/E1.png');
+  }
+
+
+  create() {
+  const startBackground = this.add.image(0, 0, 'Startbg').setOrigin(0, 0).setScale(0.9);
+
+  const ratio = window.devicePixelRatio || 1;
+  const gameWidth = this.sys.game.config.width;
+  const gameHeight = this.sys.game.config.height;
+  startBackground.setDisplaySize(gameWidth, gameHeight);
+
+
+  const E1 = this.add.image(gameWidth / 2, gameHeight / 2, 'E1').setScale(0.9 / ratio);
+
+  const weiter = this.add.image(gameWidth - 100, gameHeight - 100, 'Weiter').setScale(0.4 / ratio).setInteractive();
+
+    weiter.on('pointerdown', () => {
+      this.scene.start('DesertLevel'); // Start the JungleLevel scene
+    });
+  }
+}
