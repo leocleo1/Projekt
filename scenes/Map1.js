1676b27068bbadbaffa790a733b1e894080360f6
jelldo <jelldo@web.de>
2025-07-10 09:26:30 +0200

    map fixex 2

--- scenes/Map1.js
+++ scenes/Map1.js
@@ -11,12 +11,13 @@ export class Map1 extends Phaser.Scene {
   
 
     create() {
-      const startBackground = this.add.image(window.innerWidth /2 - 800, 0, 'Startbg').setOrigin(0, 0).setScale(0.9);
+      const startBackground = this.add.image(0, 0, 'Startbg').setOrigin(0, 0).setScale(0.9);
       const E1 = this.add.image(window.innerWidth /2 , 420, 'E1').setScale(0.5);
       const weiter = this.add.image(1300, 750, 'Weiter').setScale(0.25).setInteractive();
 
       const gameWidth = window.innerWidth
       const gameHeight = window.innerHeight;
+      startBackground.setDisplaySize(gameWidth, gameHeight);
   
       
       weiter.on('pointerdown', () => {
--- scenes/StartScene.js
+++ scenes/StartScene.js
@@ -10,12 +10,13 @@ export class StartScene extends Phaser.Scene {
     }
   
     create() {
-      const startBackground = this.add.image(window.innerWidth /2 - 800, 0, 'Startbg').setOrigin(0, 0).setScale(0.9);
+      const startBackground = this.add.image(0, 0, 'Startbg').setOrigin(0, 0).setScale(0.9);
       const logo = this.add.image(window.innerWidth /2 , 300, 'Logo').setScale(0.7);
       const start = this.add.image(window.innerWidth/2, 600, 'Start').setScale(0.7).setInteractive();
 
       const gameWidth = window.innerWidth
       const gameHeight = window.innerHeight;
+      startBackground.setDisplaySize(gameWidth, gameHeight);
   
       
       start.on('pointerdown', () => {
