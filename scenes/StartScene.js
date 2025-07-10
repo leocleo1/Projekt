a7c145d354e8f8f15ce2bff1c3b02948a3962166
jelldo <jelldo@web.de>
2025-07-10 09:22:46 +0200

    Map fix

--- scenes/StartScene.js
+++ scenes/StartScene.js
@@ -10,7 +10,7 @@ export class StartScene extends Phaser.Scene {
     }
   
     create() {
-      const startBackground = this.add.image(0, 0, 'Startbg').setOrigin(0, 0).setScale(0.9);
+      const startBackground = this.add.image(window.innerWidth /2 - 800, 0, 'Startbg').setOrigin(0, 0).setScale(0.9);
       const logo = this.add.image(window.innerWidth /2 , 300, 'Logo').setScale(0.7);
       const start = this.add.image(window.innerWidth/2, 600, 'Start').setScale(0.7).setInteractive();
 
